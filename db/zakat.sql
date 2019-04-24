dd95072e063ef591648a68c0f8f5780324c773b0
fajarpancas <fajarpancasaputra@gmail.com>
2019-04-24 18:03:01 +0700

    Update zakat.sql

--- db/zakat.sql
+++ db/zakat.sql
@@ -1,15 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 4.6.5.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 23, 2019 at 06:40 PM
--- Server version: 10.1.32-MariaDB
--- PHP Version: 7.0.30
+-- Generation Time: Apr 14, 2019 at 01:35 PM
+-- Server version: 10.1.21-MariaDB
+-- PHP Version: 7.1.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -21,6 +19,31 @@ SET time_zone = "+00:00";
 --
 -- Database: `zakat`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `akun`
+--
+
+CREATE TABLE `akun` (
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `status` tinyint(1) NOT NULL,
+  `nama` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `akun`
+--
+
+INSERT INTO `akun` (`username`, `password`, `status`, `nama`) VALUES
+('adi', '5f4dcc3b5aa765d61d8327deb882cf99', 0, 'adi maulana'),
+('admin', '5f4dcc3b5aa765d61d8327deb882cf99', 1, 'Admin'),
+('fajarpanca', '6f8de55c789477d5158eb81bdfdd3015', 0, 'fajarpancas'),
+('fajarpancas', 'be963885b1e3bd7bbe1f6f924458589a', 1, 'Fajar Panca'),
+('panca', 'c9e023417fa66e852e4d1f920c051017', 0, 'panca'),
+('username', '5f4dcc3b5aa765d61d8327deb882cf99', 0, 'user');
 
 -- --------------------------------------------------------
 
@@ -66,8 +89,7 @@ CREATE TABLE `infaq` (
 --
 
 INSERT INTO `infaq` (`id`, `username`, `nominal_infaq`, `bukti_pembayaran`, `tanggal_input`, `tanggal_bayar`, `tanggal_verifikasi`, `status`) VALUES
-(1, 'username', 1000, '', '2019-04-02', '0000-00-00', '0000-00-00', 0),
-(2, 'adi', 1000, '', '2019-04-02', '0000-00-00', '2019-04-04', 2),
+(1, 'username', 1000, 'rainy.png', '2019-04-02', '2019-04-14', '2019-04-14', 2),
 (3, 'adi', 2000, '', '2019-04-04', '0000-00-00', '0000-00-00', 0),
 (4, 'adi', 20000, '', '2019-04-04', '0000-00-00', '0000-00-00', 0),
 (5, 'adi', 323232, '', '2019-04-04', '0000-00-00', '0000-00-00', 0),
@@ -99,10 +121,6 @@ CREATE TABLE `zakat` (
 --
 
 INSERT INTO `zakat` (`id`, `username`, `nominal_gaji`, `nominal_zakat`, `bukti_pembayaran`, `tanggal_input`, `tanggal_bayar`, `tanggal_verifikasi`, `status`) VALUES
-(16, 'adi', 9000000, 225000, 'sdlc.png', '2019-04-02', '0000-00-00', '2019-04-04', 2),
-(17, 'adi', 10000, 250, 'SDLC_(1)4.png', '2019-04-02', '2019-04-04', '2019-04-04', 1),
-(18, 'adi', 600000, 15000, 'use_case_diagram.png', '2019-04-02', '2019-04-04', '2019-04-04', 1),
-(19, 'adi', 8000000, 200000, '', '2019-04-02', '0000-00-00', '2019-04-04', 0),
 (20, 'adi', 9090909, 227273, '', '2019-04-02', '0000-00-00', '0000-00-00', 0),
 (21, 'adi', 9303030, 232576, '', '2019-04-02', '0000-00-00', '0000-00-00', 0),
 (22, 'adi', 292992, 7325, '', '2019-04-02', '0000-00-00', '0000-00-00', 0),
@@ -114,14 +132,20 @@ INSERT INTO `zakat` (`id`, `username`, `nominal_gaji`, `nominal_zakat`, `bukti_p
 (28, 'adi', 10000000, 250000, '', '2019-04-04', '0000-00-00', '0000-00-00', 0),
 (29, 'adi', 10000000, 250000, '', '2019-04-04', '0000-00-00', '0000-00-00', 0),
 (30, 'adi', 113, 3, '', '2019-04-04', '0000-00-00', '0000-00-00', 0),
-(31, 'adi', 10000, 250, '', '2019-04-04', '0000-00-00', '0000-00-00', 0),
 (32, 'adi', 1000000, 25000, '', '2019-04-04', '0000-00-00', '0000-00-00', 0),
 (33, 'adi', 10000000, 250000, '', '2019-04-04', '0000-00-00', '0000-00-00', 0),
-(34, 'adi', 12, 0, '', '2019-04-04', '0000-00-00', '0000-00-00', 0);
+(34, 'adi', 12, 0, '', '2019-04-04', '0000-00-00', '0000-00-00', 0),
+(35, 'username', 1, 0, '', '2019-04-11', '0000-00-00', '0000-00-00', 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `akun`
+--
+ALTER TABLE `akun`
+  ADD PRIMARY KEY (`username`);
 
 --
 -- Indexes for table `artikel`
@@ -150,20 +174,16 @@ ALTER TABLE `zakat`
 --
 ALTER TABLE `artikel`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
 --
 -- AUTO_INCREMENT for table `infaq`
 --
 ALTER TABLE `infaq`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
-
 --
 -- AUTO_INCREMENT for table `zakat`
 --
 ALTER TABLE `zakat`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
-COMMIT;
-
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
