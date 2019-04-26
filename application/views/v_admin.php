3901e61c3cabedd7132846e557c32c7efadaefb4
fadhilshofian13 <47812850+fadhilshofian13@users.noreply.github.com>
2019-04-27 02:19:58 +0700

    Update v_admin.php
    nambah search

--- application/views/v_admin.php
+++ application/views/v_admin.php
@@ -1,3 +1,11 @@
+<?php
+if(!empty($this->session->userdata('filter'))){
+  $filter = $this->session->filter;
+}else{
+  $filter = '';
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -134,16 +142,47 @@
           </button>
 
           <!-- Topbar Search -->
-          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+          <?php if($content_view == "v_zakatAdmin.php"){?>
+          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search" method="get" action="<?php echo base_url().'index.php/c_zakat/zakatAdmin'?>">
             <div class="input-group">
-              <input type="text" class="form-control bg-light border-0 small" placeholder="Cari..." aria-label="Search" aria-describedby="basic-addon2">
+              <input type="search" class="form-control bg-light border-0 small" placeholder="Cari..." aria-label="Search" aria-describedby="basic-addon2" name="username" value="<?php echo $filter;?>">
               <div class="input-group-append">
-                <button class="btn btn-primary" type="button">
+                <button class="btn btn-primary" type="submit" name="submit">
                   <i class="fas fa-search fa-sm"></i>
                 </button>
+                <?php 
+                $attrs = array(
+                  'class' => 'btn btn-primary',
+                  'role' => 'button',
+                  'aria-pressed' => "true"
+                );
+                echo anchor('C_zakat/unset_session_search','Clear Search',$attrs);
+                ?>
               </div>
             </div>
           </form>
+          <?php } ?>
+
+          <?php if($content_view == "v_infaqAdmin.php"){?>
+          <form class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search" method="get" action="<?php echo base_url().'index.php/c_zakat/infaqAdmin'?>">
+            <div class="input-group">
+              <input type="search" class="form-control bg-light border-0 small" placeholder="Cari..." aria-label="Search" aria-describedby="basic-addon2" name="username" value="<?php echo $filter;?>">
+              <div class="input-group-append">
+                <button class="btn btn-primary" type="submit" name="submit">
+                  <i class="fas fa-search fa-sm"></i>
+                </button>
+                <?php 
+                $attrs = array(
+                  'class' => 'btn btn-primary',
+                  'role' => 'button',
+                  'aria-pressed' => "true"
+                );
+                echo anchor('C_zakat/unset_session_search_infaq','Clear Search',$attrs);
+                ?>
+              </div>
+            </div>
+          </form>
+          <?php } ?>
 
           <!-- Topbar Navbar -->
           <ul class="navbar-nav ml-auto">
